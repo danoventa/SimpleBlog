fb95d306cc4c0d9e8ec2af9216badd555566401e
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-04-13 13:15:40 -0500

    Added the authorization bones to the site.
    Used @model to select a model for the authorization. Now able to add
    strong typed variables to the forms. Validation and types are set in the
    C code. Razor files contain the forms. Data is checked in the
    controller.

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -11,7 +12,26 @@ namespace SimpleBlog.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+                // Test = "This is my test value set in my controller!"
+            });
+        }
+        
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "rainbow dash")
+            {
+                ModelState.AddModelError("Username", "UserName or Password isn't correct.");
+                return View(form);
+            }
+
+            // form.Test = "This is a value set in my post action";
+            return Content("The form is valid");
         }
     }
 }
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        [Required]
+        // public string Test { get; set; }
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
