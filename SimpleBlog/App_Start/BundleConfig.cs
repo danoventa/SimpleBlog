2d238f2b094f2831cab1fa2ca0a09e67b7bfe7c4
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-04-20 18:23:51 -0500

    Added Angular.js to play with while workin.
    Additional, not  part of the tutorial. Also added a new class for this
    course. Will need to have a different version, since it wasn't showing
    up. VIdeo covers this issue in ~11:30

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(/* BundleCollection bundles */)
+        {
+            // will need a sibstitute for Microsoft.Web.Optimization since they aren't showing on the Nuget
+        }
+    }
+}
