ba53170f256d901ce06fcb7becdc2645b74c8459
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-04-22 00:12:59 -0500

    added jquery, and bootstrap to project.
    did a little manipulations, fonts now available via bootstrap. And
    Jquery is now available to use for the site.

--- SimpleBlog/App_Start/BundleConfig.cs
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -5,15 +5,31 @@ using System.Web;
 using System.Web.Optimization;
 
 
-namespace SimpleBlog.App_Start
+namespace SimpleBlog
 {
     public class BundleConfig
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/admin/styles").Include("~/content/styles/bootstrap.css").Include("/content/styles/admin.css"));
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
 
-            bundles.Add(new StyleBundle("~/styels").Include("~/content/styles/bootstrap.css").Include("~/content/styles/site.css"));
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobrusive.js")
+                .Include("~/scripts/bootstrap.js"));
         }
     }
 }
