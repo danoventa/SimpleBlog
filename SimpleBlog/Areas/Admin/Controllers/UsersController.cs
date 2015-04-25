2dca3eeab60430484d866a961853f431c852f8fb
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-04-25 18:13:43 -0500

    Added the NHibernate, and configured it!
    It's working, most of the things in web.config file were misspellings..
    typical.

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NHibernate.Linq;
+using SimpleBlog.Areas.Admin.ViewModels;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -14,7 +17,10 @@ namespace SimpleBlog.Areas.Admin.Controllers
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return View();
+            return View(new UsersIndex
+            {
+                Users = Database.Session.Query<User>().ToList()
+            });
         }
     }
 }
