02249facdf9b0b327e744f3ae4a720e72595c9aa
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-05-09 22:34:28 -0500

    Fixed Migration issue for content

---
+++ SimpleBlog/Migrations/_003_AddContentToPosts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+using FluentMigrator.Builders.Alter.Column;
+using FluentMigrator.Expressions;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(3)]
+    public class _003_AddContentToPosts : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("content").OnTable("posts").AsCustom("TEXT");
+        }
+        public override void Down()
+        {
+            Delete.Column("content").FromTable("posts");
+        }
+
+    }
+}
